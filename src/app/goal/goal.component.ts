31cda5b26fd78f793f64bc9393697da59a2992a2
MrazTevin <millatevin@gmail.com>
2018-01-10 16:41:10 +0300

    functionality to delete goal

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -18,6 +18,11 @@ export class GoalComponent implements OnInit {
     toogleDetails(index){
       this.goals[index].showDescription = !this.goals[index].showDescription;
     }
+    completeGoal(isComplete,index){
+      if (isComplete){
+        this.goals.splice(index, 1);
+      }
+    }
   constructor() { }
 
   ngOnInit() {
